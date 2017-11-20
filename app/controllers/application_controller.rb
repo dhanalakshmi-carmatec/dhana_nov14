138c398ab12e9d6c3b176012d5164fc619a3b016
dhanalakshmi <dhanalakshmi@carmatec.in>
2017-11-20 18:29:12 +0530

    link changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery
   before_action :authenticate_user!
+  
+  def after_sign_in_path_for(resource)
+    users_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
 end
